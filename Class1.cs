4833a02d023b437eced7a28b33a7748bc11c66d6
skafula <79100340+skafula@users.noreply.github.com>
2023-07-20 16:56:03 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿public class Employee
+{
+    private int _salary;
+
+    public int Salary
+    {
+        get { return _salary; }
+        set { _salary = value; }
+    }
+}
+
+public class Student
+{
+    private int _marks;
+
+    public int Marks
+    {
+        get { return _marks; }
+        set { _marks = value; }
+    }
+}
+
+public class Print
+{
+    public void PrintValue<T>(T obj) where T : class
+    {
+        if (obj.GetType() == typeof(Student))
+        {
+            Student temp = obj as Student; //obj must be downcasted(safety with 'as') because obj is Object type as parameter
+            Console.WriteLine(temp.Marks);
+        }
+        else if (obj.GetType() == typeof(Employee))
+        {
+            Employee temp = obj as Employee;
+            Console.WriteLine(temp.Salary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Print print = new();
+
+        print.PrintValue(new Student() { Marks = 5 });
+        print.PrintValue(new Employee() { Salary = 1000 });
+    }
+}
